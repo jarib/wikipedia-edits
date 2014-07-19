6b1d6f8635be0270f6e501b48ad231f98549fafa
Jari Bakken <jari.bakken@gmail.com>
2014-07-19 21:24:24 +0200

    More app stuff, but will probably switch away from ES eventually.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var express = require('express'),
-    hbs     = require('express-hbs'),
-    logger  = require('morgan'),
-    app     = express();
+var express   = require('express'),
+    hbs       = require('express-hbs'),
+    logger    = require('morgan'),
+    es        = require('./lib/es-client'),
+    moment    = require('moment'),
+    deepmerge = require('deepmerge'),
+    app       = express();
 
 app.use(logger('short'));
 app.engine('hbs', hbs.express3({
@@ -15,8 +18,50 @@ app.set('views', __dirname + '/views');
 app.set('port', process.env.HTTP_PORT || 9898);
 app.use(express.static(__dirname + '/public'));
 
+function presentHit(hit) {
+    var revision = hit._source,
+        wiki     = hit._index.split('.')[1],
+        m        = moment(revision.timestamp);
+
+    revision.diff_url = 'http://' + wiki + '.wikipedia.org/w/index.php?diff=' + revision.revision_id;
+    revision.date = m.format("YYYY-MM-DD HH:mm");
+
+    return revision;
+}
+
+function presentSearchResults(body) {
+    //console.log(JSON.stringify(body, null, '   '));
+
+    return {
+        totalHits: body.hits.total,
+        results: body.hits.hits.map(presentHit)
+    };
+}
+
 app.get('/', function (req, res) {
     res.render('index');
+});
+
+app.get('/search', function (req, res) {
+    console.log(req.query);
+
+    var q = {
+        index: 'wiki.' + req.query.wikipedia,
+        body: {
+            sort: {'timestamp': 'desc'},
+            query: {
+                "match" : {"title" : req.query.keyword.trim().length ? req.query.keyword : '*'}
+            }
+        }
+    };
+
+    es.search(q)
+        .then(function (body) {
+            res.render('results', deepmerge(presentSearchResults(body), {query: req.query}));
+        }).catch(function (err) {
+            console.error(err, err.stack);
+            res.render('error', {error: err});
+        });
 });
 
 app.listen(app.get('port'), function () {
